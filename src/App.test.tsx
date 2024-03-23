72bbf27932efb17479d628fdcce7819b50c18b94
Marytere Benavides <maryterebenavides@gmail.com>
2024-03-23 00:28:56 -0500

    chore: create readme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Home } from './pages';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByRole('heading', { name: /Home/i });
-  expect(linkElement).toBeInTheDocument();
+test('renders Home component', () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+
+  const homeElement = screen.getByText(/Seguro Salud Flexible/i);
+  expect(homeElement).toBeInTheDocument();
 });

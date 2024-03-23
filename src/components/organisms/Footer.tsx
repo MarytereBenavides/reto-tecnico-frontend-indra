eb1533d270d9de12f6fe6d1010709d9e42073760
Marytere Benavides <maryterebenavides@gmail.com>
2024-03-22 20:54:48 -0500

    chore: create footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,7 +1,16 @@
+import { ReactComponent as RimacSVG } from '../../assets/rimac.svg';
+import Logo from '../../assets/rimac.png';
 const Footer = () => {
     return (
-        <footer>
-            <p>Footer</p>
+        <footer className="footer">
+            <div className='footer__logo'>
+                <RimacSVG className='footer__logo--web' />
+                <img src={Logo} alt="logo de rimac" className='footer__logo--mobile' />
+            </div>
+            <hr className='footer__line'/>
+            <div className='footer__information'>
+                <span className='footer__information--copyright'>© 2023 RIMAC Seguros y Reaseguros.</span>
+            </div>
         </footer>
     );
 }

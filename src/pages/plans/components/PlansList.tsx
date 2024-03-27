1c63acd3a35c1458c1f058db83b3984ecc19be45
Marytere Benavides <maryterebenavides@gmail.com>
2024-03-27 14:32:05 -0500

    fix: fix styles

--- src/pages/plans/components/PlansList.tsx
+++ src/pages/plans/components/PlansList.tsx
@@ -50,18 +50,22 @@ function PlansList({ user, selectedUserPlan }: PlansListProps) {
             {errorPlans && <p className="message_error">Ha ocurrido un error</p>}
             {isInfoPlansLoading && selectedUserPlan && <p>Cargando...</p>}
             {!isInfoPlansLoading && selectedUserPlan && plans.length > 0 && (plansForAge.map((plan: any) => (
-                <div key={plan.name} >
-                    <h2>{plan.name}</h2>
-                    <p>COSTO DEL PLAN</p>
-                    {user.planUser === "Para alguien más" && <p>{`$${plan.price} antes`}
+                <div key={plan.name} className="card card--noHover" >
+                    <div>
+                    <h2 className="plans__plans__title">{plan.name}</h2>
+                    <p className="plans__plans__costTitle">COSTO DEL PLAN</p>
+                    {user.planUser === "Para alguien más" && <p className="plans__plans__previewCost">{`$${plan.price} antes`}
                     </p>}
-                    <p>{`$${actualPrice(plan)} al mes`}
+                    <p className="plans__plans__price">{`$${actualPrice(plan)} al mes`}
                     </p>
+                    <hr className="plans__plans__line"/>
                     <ul>
                         {plan.description.map((plan: string) => (
-                            <li key={plan}>{plan}</li>
+                            <li key={plan} className="plans__plans__description">{plan}</li>
                         ))}
                     </ul>
+                    </div>
+
                     <Button type="button" onClick={() => onSelectPlan(plan)} className="button--pink" text="Seleccionar Plan" />
                 </div>
             )))}
